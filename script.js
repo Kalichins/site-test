7d18ccec37eebe2f2e7347f7ec403486d57477ec
unknown <nurkhankomek7@gmail.com>
2025-02-13 12:38:07 +0500

    Первый коммит

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    new Swiper(".mySwiper", {
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
